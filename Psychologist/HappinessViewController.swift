8997a0cb9f41c7e991c0e3537aabc08501951494
BestKora <developer@bestkora.com>
2015-04-14 08:02:58 +0300

    Для iOS 8.3

---
+++ Psychologist/HappinessViewController.swift
@@ -0,0 +1,57 @@
+//
+//  HappinessViewController.swift
+//  Happiness
+//
+//  Created by CS193p Instructor.
+//  Copyright (c) 2015 Stanford University. All rights reserved.
+//
+
+import UIKit
+
+class HappinessViewController: UIViewController, FaceViewDataSource
+{
+    var happiness: Int = 25 { // 0 = very sad, 100 = ecstatic
+        didSet {
+            happiness = min(max(happiness, 0), 100)
+            println("happiness = \(happiness)")
+            updateUI()
+        }
+    }
+    
+    func updateUI() {
+        faceView?.setNeedsDisplay()
+        title = "\(happiness)"
+    }
+    
+    // FaceViewDataSource
+    func smilinessForFaceView(sender: FaceView) -> Double? {
+        return Double(happiness-50)/50
+    }
+
+    @IBOutlet weak var faceView: FaceView! {
+        didSet {
+            faceView.dataSource = self
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "scale:"))
+
+        }
+    }
+    
+    private struct Constants {
+        static let HappinessGestureScale: CGFloat = 4
+    }
+    
+    // handler for a pan gesture recognized by our FaceView
+    @IBAction func changeHappiness(gesture: UIPanGestureRecognizer) {
+        switch gesture.state {
+        case .Ended: fallthrough
+        case .Changed:
+            let translation = gesture.translationInView(faceView)
+            let happinessChange = -Int(translation.y / Constants.HappinessGestureScale)
+            if happinessChange != 0 {
+                happiness += happinessChange
+                gesture.setTranslation(CGPointZero, inView: faceView)
+            }
+        default: break
+        }
+    }
+}
---
+++ Psychologist/TextViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TextViewController.swift
+//  Psychologist
+//
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+
+
+    @IBOutlet weak var textView: UITextView! {
+        didSet{
+          textView.text = text
+        }
+    }
+    
+    var text: String = "" {
+        didSet {
+            textView?.text = text
+        }
+    }
+    override var preferredContentSize: CGSize {
+        get {
+            if textView != nil && presentingViewController != nil {
+                return textView.sizeThatFits(presentingViewController!.view.bounds.size)
+            } else {
+                return super.preferredContentSize
+            }
+        }
+        set { super.preferredContentSize = newValue }
+    }
+
+}
